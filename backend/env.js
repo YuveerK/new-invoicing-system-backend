3e6108f7209c48ae4fd277f0acd842eec0201ef5
Yuveer Kallideen <69754708+YuveerK@users.noreply.github.com>
2024-03-23 10:08:54 +0200

    Initial Commit

---
+++ backend/env.js
@@ -0,0 +1,15 @@
+// Environment Variables
+export const ENV = {
+  MAIL_SERVICE: "hotmail",
+  MAIL_USER: "yuveerkallideen@hotmail.com",
+  MAIL_PASS: "Test@123456789",
+  CORS_ORIGINS: [
+    "http://169.1.238.73:3000",
+    "http://192.168.50.24:3000",
+    "http://localhost:3000",
+    "http://169.1.238.73",
+    "http://192.168.50.24",
+    "http://localhost",
+  ],
+  SERVER_PORT: 4000,
+};
---
+++ backend/firebase/complexes.js
@@ -0,0 +1,66 @@
+export const complexesData = [
+  {
+    complexName: "Princeton",
+    contact: {
+      name: "Yuveer",
+      email: "yuveerkal@hotmail.com",
+      contact: "0699467188",
+    },
+  },
+  {
+    complexName: "UCLA",
+    contact: {
+      name: "Tokelo",
+      email: "tokelon@gmail.com",
+      contact: "0837777188",
+    },
+  },
+  {
+    complexName: "Cambridge",
+    contact: {
+      name: "Elzhervaen L",
+      email: "elzhervaenl@gmail.com",
+      contact: "0780059026",
+    },
+  },
+  {
+    complexName: "Stanford",
+    contact: {
+      name: "Thembi",
+      email: "zkkhumalo@gmail.com",
+      contact: "0761787308",
+    },
+  },
+  {
+    complexName: "Yale",
+    contact: {
+      name: "Shy",
+      email: "shykutlwano@gmail.com",
+      contact: "0843367840",
+    },
+  },
+  {
+    complexName: "NYU",
+    contact: {
+      name: "Jackie",
+      email: "jackie.krog@gmail.com",
+      contact: "0844220096",
+    },
+  },
+  {
+    complexName: "Harvard",
+    contact: {
+      name: "Ruan",
+      email: "rvosloo21@gmail.com",
+      contact: "0823823628",
+    },
+  },
+  {
+    complexName: "Oxford",
+    contact: {
+      name: "Warren",
+      email: "warrengst@gmail.com",
+      contact: "0784590688",
+    },
+  },
+];
